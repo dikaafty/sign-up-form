e4708fb71bfa2a885c6bf2aa404fea1a060a13c4
Dika Afty <dikaafty@gmail.com>
2024-12-02 12:48:04 +0700

    Create Validate Password Function

--- main.js
+++ main.js
@@ -2,3 +2,8 @@
 const password = document.getElementById("pass");
 const confirmPassword = document.getElementById("confirm-pass");
 const invalidConfirm = document.querySelector(".invalid");
+
+// Function
+const validatePass = () => {
+  
+}
