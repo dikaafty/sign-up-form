f40cead53b97b7541773cb5460ebdf8c52abb933
Dika Afty <dikaafty@gmail.com>
2024-12-02 12:46:54 +0700

    Select Invalid Validation Element

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 // Selector
 const password = document.getElementById("pass");
 const confirmPassword = document.getElementById("confirm-pass");
+const invalidConfirm = document.querySelector(".invalid");
